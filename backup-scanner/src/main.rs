3a6994079a1616024cc214a688806b4025dfb118
Tomek <tomek7sa@gmail.com>
2017-12-10 17:20:42 +0100

    Prints files in folder with modification date

--- backup-scanner/src/main.rs
+++ backup-scanner/src/main.rs
@@ -1,3 +1,53 @@
+extern crate chrono;
+
+use std::io;
+use std::fs;
+use std::path::Path;
+use std::time::*;
+use chrono::prelude::*;
+
+//TODO make use of this function
+/*fn visit_dirs(dir: &Path, cb: &Fn(&DirEntry)) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                visit_dirs(&path, cb)?;
+            } else {
+                cb(&entry)
+            }
+        }
+    }
+    Ok(())
+}*/
+
+fn system_time_to_date_time(t: SystemTime) -> DateTime<Utc> {
+    let (sec, nsec) = match t.duration_since(UNIX_EPOCH) {
+        Ok(dur) => (dur.as_secs() as i64, dur.subsec_nanos()),
+        Err(e) => { // unlikely but should be handled
+            let dur = e.duration();
+            let (sec, nsec) = (dur.as_secs() as i64, dur.subsec_nanos());
+            if nsec == 0 {
+                (-sec, 0)
+            } else {
+                (-sec - 1, 1_000_000_000 - nsec)
+            }
+        },
+    };
+    Utc.timestamp(sec, nsec)
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Ok(entries) = fs::read_dir(".") {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                if let Ok(metadata) = entry.metadata() {
+                    println!("{:?} {:?}", entry.path(), system_time_to_date_time(metadata.modified().unwrap()));
+                } else {
+                    println!("Couldn't get metadata for {:?}", entry.path());
+                }
+            }
+        }
+    }
 }
